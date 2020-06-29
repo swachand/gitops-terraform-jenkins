5c3696e9b233ff7bf365684a8a2f061dac912d22
swachand <swachandj@gmail.com>
2020-06-30 00:00:16 +0530

    updating s3 bucket info

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-alex"
+    bucket = "terraform-bucket-swach"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
 
 # Use AWS Terraform provider
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 # Create EC2 instance
